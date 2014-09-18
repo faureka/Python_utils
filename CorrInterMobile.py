9c19ef676349bfa81f03a05b493c387e2a57b51f
faureka <faizan.peace@gmail.com>
2014-09-18 21:29:21 +0530

    Added population plot and correlation line

--- CorrInterMobile.py
+++ CorrInterMobile.py
@@ -22,6 +22,9 @@ def prettyprint(coldata,num):
 def remheader(coldata):
 	return coldata[1:len(coldata)-5]
 
+def normplotdata(coldata):
+	return coldata/(sum(coldata)/len(coldata)) * 100
+
 data = pd.read_csv('CoreIndicators.csv',header = 2)
 data = pd.DataFrame(data)
 
@@ -38,18 +41,17 @@ IndexData = remheader(IndexData)
 CountryName = data['Country Name']
 CountryName = remheader(CountryName)
 CountryName_pop = PopData['Country Name']
-AvgPopData = PopData['2012']
+AvgPop = PopData['2012']
 
 # prettyprint(data['Country Name'],4)
 popdata = [0]*len(CountryName)
 
 for i in xrange(len(CountryName)):
-	for j in xrange(len(CountryName_pop)):
+	for j in xrange(1,len(CountryName_pop)):
 		if CountryName.iloc[i].lower() == CountryName_pop.iloc[j].lower():
-			popdata[i] = AvgPopData.iloc[j]
+			popdata[i] = AvgPop.iloc[j]
 
-popdata = popdata / max(popdata)
-AvgPopData = pd.DataFrame((popdata) * 1000)
+AvgPopData = pd.DataFrame(normplotdata(popdata))
 
 CompData = inputNAN(CompData)
 InterData = inputNAN(InterData)
@@ -57,13 +59,36 @@ IndexData = inputNAN(IndexData)
 
 
 CompInterData = pd.DataFrame({'Internet %':InterData,
-							'Computer %':CompData })
-							# 'Index':IndexData })
+							'Computer %':CompData,
+							'Index':IndexData })
 
-print CompInterData['Internet %'].corr(CompInterData['Computer %'])
+slope =  CompInterData['Internet %'].corr(CompInterData['Computer %'])
 
-# # pyplot.figure(\)
-# # CompInterData.plot(kind = 'bar')
-pyplot.scatter(CompInterData['Internet %'],CompInterData['Computer %'],s = AvgPopData)
+x = np.linspace(1,100,100,endpoint = False)
+y = slope * x
+
+x_pop = np.linspace(1,len(CountryName_pop)+1, len(CountryName_pop),endpoint = False)
+meanAvgPop = sum(AvgPop)/len(AvgPop)
+y_pop = [meanAvgPop] * len(x_pop)
+NormAvgPop = normplotdata(AvgPop)
+maxAvgPop = max(NormAvgPop)
+
+col = cm.jet(np.arange(len(InterData)))
+col_pop = cm.jet(np.arange(len(x_pop)))
+# print col_pop
+fig = pyplot.figure()
+ax = fig.add_subplot(111)
+ax.scatter(CompInterData['Internet %'],CompInterData['Computer %'],s = AvgPopData, c = col)
+ax.plot(y,x,'--r',lw = 1.5)
+ax.set_xlabel('% Individual with Internet in a Country')
+ax.set_ylabel('% Individual with Mobile Connectivity')
+ax.set_title('Correlation plot of Internet Connection vs Mobile Connectivity\n(Size of bubble indicates relative population of country)')
+ax.set_axis_bgcolor('k')
+fig1 = pyplot.figure()
+ax1 = fig1.add_subplot(111)
+ax1.scatter(x_pop,y_pop , s = NormAvgPop,c = col_pop)
+ax1.set_xlim(xmin = 0,xmax = len(x_pop) + len(x_pop)*0.05)
+# ax1.set_ylim(ymax = meanAvgPop - maxAvgPop * 0.0005)
+ax1.set_axis_bgcolor('k')
 pyplot.show()
 
